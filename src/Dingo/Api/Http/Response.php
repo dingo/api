f68bddbf9a78e0d50cc965faae85a9e0ac24e1cf
Jason Lewis <jason.lewis1991@gmail.com>
2014-03-10 17:15:53 +1100

    Removing unprocessable entity automagic.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- src/Dingo/Api/Http/Response.php
+++ src/Dingo/Api/Http/Response.php
@@ -24,17 +24,6 @@ class Response extends \Illuminate\Http\Response {
 	 */
 	public function morph()
 	{
-		// If a response is an indexed array consisting of two keys then it's assumed that
-		// the first key is an error message and the second key is an instance of
-		// \Illuminate\Support\MessageBag. These responses are treated as
-		// unprocessable entities.
-		if ($this->isUnprocessableEntity())
-		{
-			list ($message, $errors) = $this->original;
-
-			$this->setStatusCode(422) and $this->original = compact('message', 'errors');
-		}
-
 		$this->headers->set('content-type', 'application/json');
 
 		return $this->morphJsonResponse();
@@ -84,32 +73,6 @@ class Response extends \Illuminate\Http\Response {
 	protected function morphArrayableInterface($content)
 	{
 		return $content instanceof ArrayableInterface ? $content->toArray() : $content;
-	}
-
-	/**
-	 * Determine if the response is an unprocessable entity.
-	 * 
-	 * @return bool
-	 */
-	protected function isUnprocessableEntity()
-	{
-		if (is_array($this->original) and count($this->original) == 2)
-		{
-			// Ensure that the array that was returned is in fact an indexed array and not
-			// an associative array. We can do this by comparing the keys of the array
-			// with the "keys of the keys". If they are not the same then we have an
-			// associative array and must return false.
-			$keys = array_keys($this->original);
-
-			if ($keys !== array_keys($keys))
-			{
-				return false;
-			}
-
-			list ($message, $errors) = $this->original;
-
-			return is_string($message) and $errors instanceof MessageBag;
-		}
 	}
 
 }
