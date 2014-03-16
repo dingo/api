0de6d0c6667c1b8a9e17c768754a024a31a3fd1f
Jason Lewis <jason.lewis1991@gmail.com>
2014-03-16 16:32:30 +1100

    Fixed bug where internal exceptions were handled and not rethrown.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- src/Dingo/Api/Routing/Router.php
+++ src/Dingo/Api/Routing/Router.php
@@ -107,7 +107,7 @@ class Router extends \Illuminate\Routing\Router {
 			// If an exception is caught and we are currently routing an API request then
 			// we'll handle this exception by building a new response from it. This
 			// allows the API to gracefully handle its own exceptions.
-			if ($this->routingForApi() or $this->requestTargettingApi($request) and ! $request instanceof InternalRequest)
+			if (($this->routingForApi() or $this->requestTargettingApi($request)) and ! $request instanceof InternalRequest)
 			{
 				$response = $this->handleException($exception);
 			}
