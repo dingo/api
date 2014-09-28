de27beadd8eb204b0d7e9ddc2ee405e8aa3ac902
Lucas Michot <lucas@semalead.com>
2014-09-28 11:09:46 +0200

    Fix alignment

--- src/Http/Middleware/Authentication.php
+++ src/Http/Middleware/Authentication.php
@@ -145,7 +145,7 @@ class Authentication implements HttpKernelInterface
     {
         $action = $route->getAction();
 
-	    return in_array('protected', $action, true) || array_get($action, 'protected') === true;
+        return in_array('protected', $action, true) || array_get($action, 'protected') === true;
     }
 
     /**
