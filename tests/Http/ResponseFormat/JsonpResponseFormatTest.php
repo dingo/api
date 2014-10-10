96f34397f01e31251b7e104a35ace6ba9b425448
Jason Lewis <jason.lewis1991@gmail.com>
2014-10-10 20:16:52 +1100

    Test coverage for JSONP response formatter.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ tests/Http/ResponseFormat/JsonpResponseFormatTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Dingo\Api\Tests\Http\ResponseFormat;
+
+use Mockery;
+use Dingo\Api\Http\Response;
+use Illuminate\Http\Request;
+use PHPUnit_Framework_TestCase;
+use Illuminate\Support\MessageBag;
+use Dingo\Api\Tests\Stubs\EloquentModelStub;
+use Illuminate\Database\Eloquent\Collection;
+use Dingo\Api\Http\ResponseFormat\JsonpResponseFormat;
+
+class JsonpResponseFormatTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $formatter = new JsonpResponseFormat;
+        $formatter->setRequest(Request::create('GET', '/', ['callback' => 'foo']));
+
+        Response::setFormatters(['json' => $formatter]);
+        Response::setTransformer(Mockery::mock('Dingo\Api\Transformer\Transformer')->shouldReceive('transformableResponse')->andReturn(false)->getMock());
+    }
+
+
+    public function tearDown()
+    {
+        Mockery::close();
+
+        Response::setFormatters([]);
+    }
+
+
+    public function testMorphingEloquentModel()
+    {
+        $response = (new Response(new EloquentModelStub))->morph();
+
+        $this->assertEquals('foo({"user":{"foo":"bar"}});', $response->getContent());
+    }
+
+
+    public function testMorphingEloquentCollection()
+    {
+        $response = (new Response(new Collection([new EloquentModelStub, new EloquentModelStub])))->morph();
+
+        $this->assertEquals('foo({"users":[{"foo":"bar"},{"foo":"bar"}]});', $response->getContent());
+    }
+
+
+    public function testMorphingEmptyEloquentCollection()
+    {
+        $response = (new Response(new Collection))->morph();
+
+        $this->assertEquals('foo([]);', $response->getContent());
+    }
+
+
+    public function testMorphingString()
+    {
+        $response = (new Response('foo'))->morph();
+
+        $this->assertEquals('foo', $response->getContent());
+    }
+
+
+    public function testMorphingArrayableInterface()
+    {
+        $messages = new MessageBag(['foo' => 'bar']);
+
+        $response = (new Response(['foo' => 'bar', 'baz' => $messages]))->morph();
+
+        $this->assertEquals('foo({"foo":"bar","baz":{"foo":["bar"]}});', $response->getContent());
+    }
+
+
+    public function testMorphingUnknownType()
+    {
+        $this->assertEquals(1, (new Response(1))->morph()->getContent());
+    }
+
+}
