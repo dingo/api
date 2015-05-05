fd857f082ef0814fa61fb9a07358fddee49074cd
Jason Lewis <jason.lewis1991@gmail.com>
2015-05-05 18:07:23 +1000

    Fix up the parser interface by type hinting the Illuminate request instance.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- src/Http/Parser/ParserInterface.php
+++ src/Http/Parser/ParserInterface.php
@@ -2,14 +2,14 @@
 
 namespace Dingo\Api\Http\Parser;
 
-use Dingo\Api\Http\Request;
+use Illuminate\Http\Request;
 
 interface ParserInterface
 {
     /**
      * Parse an incoming request.
      *
-     * @param  \Dingo\Api\Http\Request  $request
+     * @param  \Illuminate\Http\Request  $request
      * @return mixed
      */
     public function parse(Request $request);
