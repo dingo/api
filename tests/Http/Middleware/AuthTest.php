ae030eb5263c1c2178e2ce24d0e0788642d548b7
Jason Lewis <jason.lewis1991@gmail.com>
2015-05-17 08:58:02 +1000

    Test coverage for middlewares.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ tests/Http/Middleware/AuthTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Dingo\Api\Tests\Http\Middleware;
+
+use Mockery as m;
+use Dingo\Api\Http\Request;
+use PHPUnit_Framework_TestCase;
+use Illuminate\Container\Container;
+use Dingo\Api\Http\Middleware\Auth;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class AuthTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->router = m::mock('Dingo\Api\Routing\Router');
+        $this->auth = m::mock('Dingo\Api\Auth\Auth');
+        $this->middleware = new Auth($this->router, $this->auth);
+    }
+
+    public function testProtectedRouteFiresAuthenticationAndPasses()
+    {
+        $request = Request::create('test', 'GET');
+
+        $route = m::mock('Dingo\Api\Routing\Route');
+        $route->shouldReceive('isProtected')->once()->andReturn(true);
+        $route->shouldReceive('getAuthProviders')->once()->andReturn([]);
+
+        $this->auth->shouldReceive('check')->once()->with(false)->andReturn(false);
+        $this->auth->shouldReceive('authenticate')->once()->with([])->andReturn(null);
+
+        $this->router->shouldReceive('getCurrentRoute')->once()->andReturn($route);
+
+        $this->middleware->handle($request, function ($handledRequest) use ($request) {
+            $this->assertEquals($handledRequest, $request);
+        });
+    }
+
+    public function testProtectedRouteAlreadyLoggedIn()
+    {
+        $request = Request::create('test', 'GET');
+
+        $route = m::mock('Dingo\Api\Routing\Route');
+        $route->shouldReceive('isProtected')->once()->andReturn(true);
+
+        $this->auth->shouldReceive('check')->once()->with(false)->andReturn(true);
+
+        $this->router->shouldReceive('getCurrentRoute')->once()->andReturn($route);
+
+        $this->middleware->handle($request, function ($handledRequest) use ($request) {
+            $this->assertEquals($handledRequest, $request);
+        });
+    }
+
+    /**
+     * @expectedException \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException
+     */
+    public function testAuthenticationFailsAndExceptionIsThrown()
+    {
+        $exception = new UnauthorizedHttpException('test');
+
+        $request = Request::create('test', 'GET');
+
+        $route = m::mock('Dingo\Api\Routing\Route');
+        $route->shouldReceive('isProtected')->once()->andReturn(true);
+        $route->shouldReceive('getAuthProviders')->once()->andReturn([]);
+
+        $this->auth->shouldReceive('check')->once()->with(false)->andReturn(false);
+        $this->auth->shouldReceive('authenticate')->once()->with([])->andThrow($exception);
+
+        $this->router->shouldReceive('getCurrentRoute')->once()->andReturn($route);
+
+        $this->middleware->handle($request, function () {
+            //
+        });
+    }
+}
