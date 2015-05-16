4a404b0ca4f825d476853ba0065902ca24c4efff
Jason Lewis <jason.lewis1991@gmail.com>
2015-05-17 00:18:48 +1000

    Test coverage for route.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ tests/Routing/RouteTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Dingo\Api\Tests\Routing;
+
+use Mockery as m;
+use Dingo\Api\Http\Request;
+use Dingo\Api\Routing\Route;
+use PHPUnit_Framework_TestCase;
+use Illuminate\Container\Container;
+use Dingo\Api\Tests\Stubs\RoutingAdapterStub;
+
+class RouteTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->adapter = new RoutingAdapterStub;
+        $this->container = new Container;
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testCreatingNewRoute()
+    {
+        $request = Request::create('foo', 'GET');
+
+        $route = new Route($this->adapter, $this->container, $request, [
+            'uri' => 'foo',
+            'methods' => ['GET', 'HEAD'],
+            'action' => [
+                'scopes' => ['foo', 'bar'],
+                'protected' => false,
+                'providers' => ['foo'],
+                'limit' => 5,
+                'expires' => 10,
+                'throttle' => 'Foo',
+                'version' => ['v1'],
+                'conditionalRequest' => false
+            ]
+        ]);
+
+        $this->assertEquals(['foo', 'bar'], $route->scopes(), 'Route did not setup scopes correctly.');
+        $this->assertFalse($route->isProtected(), 'Route did not setup protection correctly.');
+        $this->assertEquals(['foo'], $route->getAuthProviders(), 'Route did not setup authentication providers correctly.');
+        $this->assertEquals(5, $route->getRateLimit(), 'Route did not setup rate limit correctly.');
+        $this->assertEquals(10, $route->getRateExpiration(), 'Route did not setup rate limit expiration correctly.');
+        $this->assertTrue($route->hasThrottle(), 'Route did not setup throttle correctly.');
+        $this->assertEquals('Foo', $route->getThrottle(), 'Route did not setup throttle correctly.');
+        $this->assertFalse($route->requestIsConditional(), 'Route did not setup conditional request correctly.');
+    }
+
+    public function testControllerOptionsMergeAndOverrideRouteOptions()
+    {
+        $request = Request::create('foo', 'GET');
+
+        $route = new Route($this->adapter, $this->container, $request, [
+            'uri' => 'foo',
+            'methods' => ['GET', 'HEAD'],
+            'action' => [
+                'scopes' => ['foo', 'bar'],
+                'protected' => false,
+                'providers' => ['foo'],
+                'limit' => 5,
+                'expires' => 10,
+                'throttle' => 'Foo',
+                'version' => ['v1'],
+                'conditionalRequest' => false,
+                'uses' => 'Dingo\Api\Tests\Stubs\RoutingControllerStub@index'
+            ]
+        ]);
+
+        $this->assertEquals(['foo', 'bar', 'baz', 'bing'], $route->scopes(), 'Route did not setup scopes correctly.');
+        $this->assertTrue($route->isProtected(), 'Route did not setup protection correctly.');
+        $this->assertEquals(['foo', 'red', 'black'], $route->getAuthProviders(), 'Route did not setup authentication providers correctly.');
+        $this->assertEquals(10, $route->getRateLimit(), 'Route did not setup rate limit correctly.');
+        $this->assertEquals(20, $route->getRateExpiration(), 'Route did not setup rate limit expiration correctly.');
+        $this->assertTrue($route->hasThrottle(), 'Route did not setup throttle correctly.');
+        $this->assertEquals('Zippy', $route->getThrottle(), 'Route did not setup throttle correctly.');
+    }
+}
---
+++ tests/Stubs/RoutingControllerStub.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dingo\Api\Tests\Stubs;
+
+use Dingo\Api\Routing\Helpers;
+
+class RoutingControllerStub
+{
+    use Helpers;
+
+    public function __construct()
+    {
+        $this->scopes('baz|bing');
+        $this->scopes('bob', ['except' => ['index']]);
+
+        $this->protect();
+        $this->unprotect(['except' => ['index']]);
+
+        $this->authenticateWith('red|black', ['only' => ['index']]);
+
+        $this->rateLimit(10, 20);
+
+        $this->throttle('Zippy');
+    }
+
+    public function index()
+    {
+        return 'foo';
+    }
+}
