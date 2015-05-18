154516cfd6bffbea2752a7e86a20170f6d1ab79b
Vincent Klaiber <vincentklaiber@gmail.com>
2015-05-18 08:22:50 +0200

    Fix corresponding typo

--- config/api.php
+++ config/api.php
@@ -98,7 +98,7 @@ return [
     |
     | When some HTTP exceptions are not caught and dealt with the API will
     | generate a generic error response in the format provided. Any
-    | keys that aren't replaced with corrosponding values will be
+    | keys that aren't replaced with corresponding values will be
     | removed from the final response.
     |
     */
