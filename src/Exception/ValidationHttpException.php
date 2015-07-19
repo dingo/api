a76ce2fed178e2f56775666b7ae35b4afabd8a22
Jason Lewis <jason.lewis1991@gmail.com>
2015-07-19 13:50:32 +1000

    Add new validation HTTP exception that only accepts validation errors.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

---
+++ src/Exception/ValidationHttpException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dingo\Api\Exception;
+
+class ValidationHttpException extends ResourceException
+{
+    /**
+     * Create a new validation HTTP exception instance.
+     *
+     * @param \Illuminate\Support\MessageBag|array $errors
+     * @param \Exception                           $previous
+     * @param array                                $headers
+     * @param int                                  $code
+     *
+     * @return void
+     */
+    public function __construct($errors = null, Exception $previous = null, $headers = [], $code = 0)
+    {
+        parent::__construct(null, $errors, $exception, $headers, $code);
+    }
+}
