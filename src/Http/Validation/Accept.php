54f5d69fb3ac60d9088db0e9e6503fb7cddcb785
Jason Lewis <jason.lewis1991@gmail.com>
2015-08-12 12:15:08 +1000

    Allow OPTIONS requests to bypass strict checking of accept header.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- src/Http/Validation/Accept.php
+++ src/Http/Validation/Accept.php
@@ -5,6 +5,7 @@ namespace Dingo\Api\Http\Validation;
 use Illuminate\Http\Request;
 use Dingo\Api\Contract\Http\Validation\Validator;
 use Dingo\Api\Http\Parser\Accept as AcceptParser;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class Accept implements Validator
 {
@@ -46,10 +47,18 @@ class Accept implements Validator
      *
      * @throws \Symfony\Component\HttpKernel\Exception\BadRequestHttpException
      *
-     * @return void
+     * @return bool
      */
     public function validate(Request $request)
     {
-        $this->accept->parse($request, $this->strict);
+        try {
+            $this->accept->parse($request, $this->strict);
+        } catch (BadRequestHttpException $exception) {
+            if ($request->getMethod() === 'OPTIONS') {
+                return true;
+            }
+
+            throw $exception;
+        }
     }
 }
