172ea9501941666fc65c282ea0b915c391b0190b
Jason Lewis <jason.lewis1991@gmail.com>
2015-08-20 11:24:04 +1000

    Use has instead of contains to check the controller class name does not already exist. Relates to #590.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- src/Console/Command/Docs.php
+++ src/Console/Command/Docs.php
@@ -149,7 +149,7 @@ class Docs extends Command
         foreach ($this->router->getRoutes() as $collections) {
             foreach ($collections as $route) {
                 if ($controller = $route->getController()) {
-                    if (! $controllers->contains(get_class($controller))) {
+                    if (! $controllers->has(get_class($controller))) {
                         $controllers->put(get_class($controller), $controller);
                     }
                 }
