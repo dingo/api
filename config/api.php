80a92610a40215638b4c31fa97b3f514199dd691
Tom Schlick <tom@tomschlick.com>
2015-10-08 19:58:59 -0400

    typo

--- config/api.php
+++ config/api.php
@@ -13,7 +13,7 @@ return [
     | (prs), and the Unregistered tree (x).
     |
     | By default the Unregistered tree (x) is used, however, should you wish
-    | to you can regsiter your type with the IANA. For more details:
+    | to you can register your type with the IANA. For more details:
     | https://tools.ietf.org/html/rfc6838
     |
     */
