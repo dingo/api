92863dfd4c0cf1ade0ea627fcd9922bd159b0c12
Caleb Fidecaro <calebfidecaro@gmail.com>
2015-12-02 10:12:08 +1300

    Exclude 'create' and 'edit' from resource routes

--- src/Routing/ResourceRegistrar.php
+++ src/Routing/ResourceRegistrar.php
@@ -7,6 +7,13 @@ use Illuminate\Routing\ResourceRegistrar as IlluminateResourceRegistrar;
 class ResourceRegistrar extends IlluminateResourceRegistrar
 {
     /**
+     * The default actions for a resourceful controller. Excludes 'create' and 'edit'.
+     *
+     * @var array
+     */
+    protected $resourceDefaults = ['index', 'store', 'show', 'update', 'destroy'];
+
+    /**
      * Create a new resource registrar instance.
      *
      * @param \Dingo\Api\Routing\Router $router
