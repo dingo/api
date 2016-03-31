5e1c0839067e82fdfa8472eb2e3f024a4c3b96ea
Jason Lewis <jason.lewis1991@gmail.com>
2016-03-31 21:33:29 +1030

    Add the --include-path option for the documentation generation.
    This follows some recent commits to the Blueprint repo. By default
    the include path is the root directory.

--- src/Console/Command/Docs.php
+++ src/Console/Command/Docs.php
@@ -54,7 +54,8 @@ class Docs extends Command
      */
     protected $signature = 'api:docs {--name= : Name of the generated documentation}
                                      {--use-version= : Version of the documentation to be generated}
-                                     {--output-file= : Output the generated documentation to a file}';
+                                     {--output-file= : Output the generated documentation to a file}
+                                     {--include-path= : Path where included documentation files are located}';
 
     /**
      * The console command description.
@@ -92,7 +93,7 @@ class Docs extends Command
      */
     public function handle()
     {
-        $contents = $this->blueprint->generate($this->getControllers(), $this->getDocName(), $this->getVersion());
+        $contents = $this->blueprint->generate($this->getControllers(), $this->getDocName(), $this->getVersion(), $this->getIncludePath());
 
         if ($file = $this->option('output-file')) {
             $this->writer->write($contents, $file);
@@ -119,6 +120,16 @@ class Docs extends Command
         }
 
         return $name;
+    }
+
+    /**
+     * Get the include path for documentation files.
+     *
+     * @return string
+     */
+    protected function getIncludePath()
+    {
+        return rtrim($this->laravel['path.base'].'/'.$this->option('include-path'), '/');
     }
 
     /**
