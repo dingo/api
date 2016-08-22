7e578d2fe981be41f31d3bbd8b7333f075addf69
Dylan Anderson <dlnandrsn@users.noreply.github.com>
2016-08-23 09:42:42 +1000

    Fixed construct method comment in the custom rate limit exceeded exception

--- src/Exception/RateLimitExceededException.php
+++ src/Exception/RateLimitExceededException.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class RateLimitExceededException extends HttpException
 {
     /**
-     * Create a new unknown version exception instance.
+     * Create a new rate limit exceeded exception instance.
      *
      * @param string     $message
      * @param \Exception $previous
