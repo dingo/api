7c38a99082814b4d861a4de1ae4f13390ef8d4ae
Meysam <p.g.meysam@gmail.com>
2017-05-07 13:49:19 +0430

    Fix 'Illegal offset type' error for non-object argument passed to hasBinding
    In situations that the parameter passed to hasBinding of Transformer for
    example is an array (suppose paginate a set of arrays and then the first() of
    the collection is an array and it's illegal for offset of ::$bindings) and it
    causes a fatal error. This pull request fix it.

--- src/Transformer/Factory.php
+++ src/Transformer/Factory.php
@@ -176,7 +176,7 @@ class Factory
 
         $class = is_object($class) ? get_class($class) : $class;
 
-        return isset($this->bindings[$class]);
+        return is_scalar($class) && isset($this->bindings[$class]);
     }
 
     /**
