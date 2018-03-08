aa16043f2d8a07fca559968e4aa30d5b546ac024
Gazder Bence <bencegazder@gmail.com>
2018-03-08 12:13:44 +0100

    Add getMiddleware function to Route class (#1537)
    * Add getMiddleware function to Route class
    
    The getMiddleware function is missing and reported here: https://github.com/flipboxstudio/lumen-generator/issues/25
    Also I think you have to be able to query the middlewares.
    
    If the function is not necessary and you can query the middlewares with another method, please write it in the comments.
    
    * Fix style
    
    * Fix style

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -533,4 +533,14 @@ class Route extends \Illuminate\Routing\Route
     {
         return in_array('https', $this->action, true);
     }
+
+    /**
+     * Return the middlewares for this route.
+     *
+     * @return array
+     */
+    public function getMiddleware()
+    {
+        return $this->middleware;
+    }
 }
