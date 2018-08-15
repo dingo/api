b05f64696554d5b69fee0cf2af8d77ebb4b76562
Gazder Bence <bencegazder@gmail.com>
2018-08-16 00:51:59 +0200

    Revert ResourceRegistrar@register (#1580)
    * Revert ResourceRegistrar@register
    
    The two breaking change:
    https://github.com/illuminate/routing/commit/660cd7174334ce0d59dfff2ac1e846a8c5dc43ac#diff-c6855a9f8a00a1c3b95fee1c42f0ff1e
    https://github.com/illuminate/routing/commit/828a5527a7b8bfb07619aa140626f8798b969f33
    
    * Add Illuminate\Support\Str import
    
    * Fix style ci config
    
    * Fix style CI

--- src/Routing/ResourceRegistrar.php
+++ src/Routing/ResourceRegistrar.php
@@ -2,6 +2,7 @@
 
 namespace Dingo\Api\Routing;
 
+use Illuminate\Support\Str;
 use Illuminate\Routing\ResourceRegistrar as IlluminateResourceRegistrar;
 
 class ResourceRegistrar extends IlluminateResourceRegistrar
@@ -23,5 +24,41 @@ class ResourceRegistrar extends IlluminateResourceRegistrar
     public function __construct(Router $router)
     {
         $this->router = $router;
+    }
+
+    /**
+     * Route a resource to a controller.
+     *
+     * @param  string  $name
+     * @param  string  $controller
+     * @param  array   $options
+     *
+     * @return void
+     */
+    public function register($name, $controller, array $options = [])
+    {
+        if (isset($options['parameters']) && ! isset($this->parameters)) {
+            $this->parameters = $options['parameters'];
+        }
+
+        // If the resource name contains a slash, we will assume the developer wishes to
+        // register these resource routes with a prefix so we will set that up out of
+        // the box so they don't have to mess with it. Otherwise, we will continue.
+        if (Str::contains($name, '/')) {
+            $this->prefixedResource($name, $controller, $options);
+
+            return;
+        }
+
+        // We need to extract the base resource from the resource name. Nested resources
+        // are supported in the framework, but we need to know what name to use for a
+        // place-holder on the route parameters, which should be the base resources.
+        $base = $this->getResourceWildcard(last(explode('.', $name)));
+
+        $defaults = $this->resourceDefaults;
+
+        foreach ($this->getResourceMethods($defaults, $options) as $m) {
+            $this->{'addResource'.ucfirst($m)}($name, $base, $controller, $options);
+        }
     }
 }
