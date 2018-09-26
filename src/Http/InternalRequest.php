84921ec05c266705928998a47888971cfa7c30cc
Max Snow <max@covergenius.biz>
2018-09-26 17:05:43 +1000

    Fixes a problem whereby parameters passed through internal requests are not available in Laravel's $request->input()

--- src/Http/InternalRequest.php
+++ src/Http/InternalRequest.php
@@ -4,5 +4,14 @@ namespace Dingo\Api\Http;
 
 class InternalRequest extends Request
 {
-    //
+    public function __construct(array $query = array(), array $request = array(), array $attributes = array(), array $cookies = array(), array $files = array(), array $server = array(), $content = null)
+    {
+        parent::__construct($query, $request, $attributes, $cookies, $files, $server, $content);
+
+        // Pass parameters inside internal request into Laravel's JSON ParameterBag,
+        // so that they can be accessed using $request->input()
+        if ($this->isJson() && isset($this->request)) {
+            $this->setJson($this->request);
+        }
+    }
 }
