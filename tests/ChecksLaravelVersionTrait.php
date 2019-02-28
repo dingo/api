24122caf003fa07ebbf28c49021591ce99e26c82
Dan Pock <dpock32509@gmail.com>
2019-02-28 15:38:47 -0500

    More CS fixes

--- tests/ChecksLaravelVersionTrait.php
+++ tests/ChecksLaravelVersionTrait.php
@@ -7,8 +7,7 @@ use Dingo\Api\Tests\Stubs\Application58Stub;
 
 trait ChecksLaravelVersionTrait
 {
-
-    public $installed_file_path = __DIR__ . '/../vendor/composer/installed.json';
+    public $installed_file_path = __DIR__.'/../vendor/composer/installed.json';
     public $current_release = '5.8';
 
     private function getFrameworkVersion()
@@ -16,13 +15,14 @@ trait ChecksLaravelVersionTrait
         $contents = file_get_contents($this->installed_file_path);
         $parsed_data = json_decode($contents, true);
         $just_laravel = array_filter($parsed_data, function ($val) {
-            if ("laravel/framework" === $val['name'] || "laravel/lumen-framework" === $val['name']) {
+            if ('laravel/framework' === $val['name'] || 'laravel/lumen-framework' === $val['name']) {
                 return true;
             }
         });
         $laravelVersion = array_map(function ($val) {
             return $val['version'];
         }, array_values($just_laravel))[0];
+
         return $laravelVersion;
     }
 
@@ -33,9 +33,11 @@ trait ChecksLaravelVersionTrait
             $version = $this->current_release;
         }
         $compared_versions = version_compare('5.8', $version);
+        // If comparison is Less Than, or Equal To, provide the 5.8 stub.
         if ($compared_versions === -1 || $compared_versions === 0) {
             return new Application58Stub;
         }
+
         return new ApplicationStub;
     }
 }
